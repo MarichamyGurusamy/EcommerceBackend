4f3d5dc291cd3672c4821fe04cd8f560514f98c1
marichamy <gmarichamy45@gmail.com>
2024-01-27 22:37:53 +0530

    Remove Extra folder

--- e-commerce-b2c/src/main/java/com/youtube/ecommerce/dao/ProductDao.java
+++
@@ -1,7 +0,0 @@
-package com.youtube.ecommerce.dao;
-
-import com.youtube.ecommerce.entity.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductDao extends CrudRepository<Product, Integer> {
-}
